8470bdd8624d96da93039b1539c1a6b27d4e6914
Rakshitha-Umesh-Patil <rakshithapatil2004@gmail.com>
2025-07-03 23:01:53 +0530

    mysql_crud operations

---
+++ mysql/mysql_crud.py
@@ -0,0 +1,67 @@
+import pymysql
+
+def connect_db():
+    connection = None
+    try:
+        connection = pymysql.Connect(
+            host='localhost',
+            user="root",
+            password="root@123",
+            database='raks_db',  # corrected spelling from 'datebase' to 'database'
+            port=3306,
+            charset="utf8"
+        )
+        print('Database Connected')
+    except:
+        print('Database Connection Failed')
+    return connection
+
+def disconnect_db(connection):
+    try:
+        connection.close()
+        print('DB disconnected')
+    except:
+        print('DB disconnection failed')
+
+def create_db():
+    query = 'create database IF NOT EXISTS nithin_db'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print('Database created')
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Database creation failed')
+
+def create_table():
+    query = 'create table IF NOT EXISTS employees(id int primary key auto_increment, name varchar(50) not null, designation varchar(30), phone_number	bigint unique, salary float, commission		float default(0), years_of_experience tinyint, technology		varchar(30)	not null)'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print('Table created')
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Table creation failed')
+
+def read_all_employees():
+    query = 'select * from employees'
+    connection = connect_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()  # corrected from fetchAll() to fetchall()
+        for row in rows:
+            print(row)
+        print('All rows retrieved')  # corrected spelling from 'retrived'
+        cursor.close()
+        disconnect_db(connection)
+    except:
+        print('Rows retrieval failed')  # corrected spelling from 'retrival'
+
+connection = connect_db()
+create_table()
+disconnect_db(connection)
